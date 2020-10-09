9fc9bcac8fc332c7dd39ecb360c3584a2447d935
Philip Lee <philip.lee@thecorporation.co.kr>
2020-10-09 15:50:35 +0900

    feat: create Aha Token contract conforms to ERC-777

---
+++ contracts/AhaToken.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+import "@openzeppelin/contracts/token/ERC777/ERC777.sol";
+
+contract AhaToken is ERC777 {
+    constructor(uint256 initialSupply, address[] memory defaultOperators)
+        public
+        ERC777("Aha Knowledge Token", "AHT", defaultOperators)
+    {
+        _mint(msg.sender, initialSupply, "", "");
+    }
+}
